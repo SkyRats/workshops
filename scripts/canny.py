8777953b0d5e3e31f0abeee0363715cae99c1321
Bárbara_Bueno <65515809+Buenobarbie@users.noreply.github.com>
2022-08-11 15:36:00 -0300

    Create canny.py

---
+++ scripts/canny.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+img = cv.imread('./images/auau.jpg',0)
+edges = cv.Canny(img,100,200)
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.show()
